0357ba222ab4df8e38dbc071a7095d5e0d16d9c9
VuVanDao <dao28901@gmail.com>
2025-09-06 21:17:06 +0700

    create support dashboard file

--- fe-bookingcare/src/routes/AppRoutes.tsx
+++ fe-bookingcare/src/routes/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "../pages/Dashboard/Dashboard";
+import Dashboard from "../pages/Dashboard/AdminDashboard/Dashboard";
 import Statistics from "../pages/Statistics/Statistics";
 import ServiceList from "../pages/Services/ServicesList/ServiceList";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
@@ -8,6 +8,7 @@ import BillPage from "../pages/Bill/BillPage";
 import BookingPage from "../pages/BookingPage/BookingPage";
 import BookingManage from "../pages/Doctor/BookingManage";
 import DoctorDashboard from "../pages/Dashboard/DoctorDashboard/DoctorDashboard";
+import SupportDashboard from "../pages/Dashboard/SupportDashboard/SupportDashboard";
 
 const AppRoutes = () => {
   return (
@@ -34,6 +35,14 @@ const AppRoutes = () => {
           element={<Navigate to={"booking-manage"} replace={true} />}
         />
         <Route path="booking-manage" element={<BookingManage />} />
+      </Route>
+
+      <Route path="/support-dashboard" element={<SupportDashboard />}>
+        <Route
+          path="/support-dashboard"
+          element={<Navigate to={"booking-support-manage"} replace={true} />}
+        />
+        <Route path="booking-support-manage" element={<BookingManage />} />
       </Route>
       <Route path="/error-page" element={<ErrorPage />} />
       <Route
